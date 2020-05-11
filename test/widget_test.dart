6cbff073f9b1404ddb3058e147776e47d0c15048
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-05-11 10:39:36 -0300

    inicia projeto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:elul/main.dart';
+import 'package:Elul/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
